931f2bb562cb89cb4eee0b9430314385cde240aa
kiminwha9465-afk <kiminwha9465@gmail.com>
2026-07-23 20:09:37 +0900

    Tailwind CDN으로 화면 디자인 적용

--- src/main/java/com/example/studynote/StudynoteApplication.java
+++ src/main/java/com/example/studynote/StudynoteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudynoteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StudynoteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StudynoteApplication.class, args);
+	}
 
 }
